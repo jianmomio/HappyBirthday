a07237c1daff953342cf210c69d2c4014b305919
jianmomio <91527979+jianmomio@users.noreply.github.com>
2023-10-31 22:12:05 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-$('#login-button').click(function (event) {
+$('#login-button').click(function (event) { //检测点击 id为“login-button”的按钮（进入），跳转页面
 	var userName=document.getElementById("userName").value;
     var pwd=document.getElementById("pwd").value;
 		//修改密码请改此处
@@ -6,7 +6,7 @@ $('#login-button').click(function (event) {
 			event.preventDefault();
 			$('form').fadeOut(500);
 			$('.wrapper').addClass('form-success');
-			setTimeout(function(){location.href="BirthdayCake.html";},2000);
+			setTimeout(function(){location.href="BirthdayCake.html";},2000);//登陆成功，跳转进下一个页面
 		}
 	else{
 		alert("Wrong Password");
